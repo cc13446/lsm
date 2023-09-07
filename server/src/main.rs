62daaf0710bab3921fa9c05ae38df30220addfca
cc13446 <1344613920@qq.com>
2023-09-08 00:58:34 +0800

    rust env config

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,85 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use log::info;
+use std::env;
+use std::process::exit;
+use serde_derive::Deserialize;
+use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+
+const SUB: &str = "-";
+
+// 文件配置参数
+#[derive(Deserialize)]
+struct FileConfig {
+    ip: String,
+    port: u32,
+}
+
+// 命令行参数
+struct EnvConfig {
+    config_file_path: String,
+}
+
+
+fn get_config_file_path(args_map: &HashMap<String, String>, default: &String) -> String {
+    String::from(args_map.get("-f").unwrap_or(args_map.get("--config-file").unwrap_or(default)))
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // init logger
+    env_logger::init();
+
+    // parse env config
+    let args: Vec<String> = env::args().collect();
+    let mut args_map: HashMap<String, String> = HashMap::new();
+
+    for i in 0..args.len() {
+        if args[i] == (String::from(SUB) + SUB) {
+            break;
+        }
+        if args[i].starts_with(SUB) && i < args.len() - 1 && !args[i + 1].starts_with(SUB) {
+            args_map.insert(args[i].clone(), args[i + 1].clone());
+        }
+    }
+
+    let mut env_config = EnvConfig {
+        config_file_path: String::from(get_config_file_path(&args_map, &String::from("./server_config.toml"))),
+    };
+
+    info!("LSM server start with config");
+    info!("LSM server config file path {}", env_config.config_file_path);
+
+
+    let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap_or_else(|err| {
+        eprintln!("Fail to open tcp server; err = {:?}", err);
+        exit(0);
+    });
+
+    loop {
+        let (mut socket, _) = listener.accept().await?;
+
+        tokio::spawn(async move {
+            let mut buf = [0; 1024];
+
+            // In a loop, read data from the socket and write the data back.
+            loop {
+                let n = match socket.read(&mut buf).await {
+                    // socket closed
+                    Ok(n) if n == 0 => return,
+                    Ok(n) => n,
+                    Err(e) => {
+                        eprintln!("failed to read from socket; err = {:?}", e);
+                        return;
+                    }
+                };
+
+                // Write the data back
+                if let Err(e) = socket.write_all(&buf[0..n]).await {
+                    eprintln!("failed to write to socket; err = {:?}", e);
+                    return;
+                }
+            }
+        });
+    }
 }
